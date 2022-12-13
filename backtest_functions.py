5e806199a3580194f05eb625ab471551b51d264e
BFord94 <billyford@stacies-MacBook-Pro.local>
2022-12-13 20:33:49 +0000

    Adding backtesting

--- backtest_functions.py
+++ backtest_functions.py
@@ -1,3 +1,38 @@
 from functions_import import *
 from stock_classes import *
 
+def var_exceptions_count(stock_name, start_date, end_date,\
+						 n_years, percentile):
+	"""
+	Count the number of daily VaR exceptions for given backtesting set
+	Parameters:
+		stock_name - Name of the stock to backtest
+		start_date - Starting date of the backtesting window
+		end_date - Final date of the backtesting window
+		n_years - Number of years of historical data to use 
+				  for VaR calculation
+		percentile - Percentile VaR to compute
+	"""
+	# Need data to cover the backtesting window and VaR calculation
+	# on day 0 of BT window
+	start_date = pd.to_datetime(start_date, format='%d/%m/%Y')
+	var_start_date = start_date - datetime.timedelta(days = n_years*365)
+	stock = StockData(stock_name)
+	stock.get_data(var_start_date, end_date)
+	# Compute daily returns
+	stock.data["Returns"] = stock.data["Adj Close"] \
+							- stock.data["Adj Close"].shift(1)
+	stock.data["VaR"] = stock.data["Returns"].rolling(n_years*365).\
+						quantile(1-percentile/100)
+	n_exceptions = len(stock.data[stock.data["Returns"] < stock.data["VaR"]])
+	return n_exceptions
+
+def generate_binomial_distribution(n, p):
+	"""
+	Generate a binomial distribution 
+	Parameters:
+		n - Total number of trials
+		p - Probability of the first outcome
+	"""
+	# Initialise x values
+	x_list = [i for i in range(n)]
