2c5cf032d42ba5791ea8815b255ee322d60afcdd
BFord94 <billyford@stacies-MacBook-Pro.local>
2022-12-21 17:54:24 +0000

    Added backtesting p-value calculation

--- backtest_functions.py
+++ backtest_functions.py
@@ -20,11 +20,11 @@ def var_exceptions_count(stock_name, start_date, end_date,\
 	stock = StockData(stock_name)
 	stock.get_data(var_start_date, end_date)
 	# Compute daily returns
-	stock.data["Returns"] = stock.data["Adj Close"] \
-							- stock.data["Adj Close"].shift(1)
-	stock.data["VaR"] = stock.data["Returns"].rolling(n_years*365).\
+	stock.data.loc[:,"Returns"] = stock.data.loc[:,"Adj Close"] \
+							- stock.data.loc[:,"Adj Close"].shift(1)
+	stock.data.loc[:,"VaR"] = stock.data.loc[:,"Returns"].rolling(n_years*365).\
 						quantile(1-percentile/100)
-	n_exceptions = len(stock.data[stock.data["Returns"] < stock.data["VaR"]])
+	n_exceptions = len(stock.data[stock.data.loc[:,"Returns"] < stock.data.loc[:,"VaR"]])
 	return n_exceptions
 
 def generate_binomial_distribution(n, p):
@@ -34,12 +34,35 @@ def generate_binomial_distribution(n, p):
 		n - Total number of trials
 		p - Probability of the first outcome
 	"""
-	# Initialise x values
-	x = np.arange(binom.ppf(0.01, n, p), binom.ppf(0.99, n, p))
 	# Generate binomial distribution
 	binomial = binom(n, p)
+	return binomial
+
+def compute_p_value(n_observations, n_exceptions, p):
+	"""
+	Compute the p-value representing the probability of obtaining less than the
+	observed number of backtesting exceptions
+	Parameters:
+		n_observations - Total number of trials, i.e. length of lookback
+						 window in days
+		n_exceptions - Number of observed backtesting exceptions
+		p - Probability of a backtesting exception, i.e. one minus the
+			VaR percentile
+	"""
+	# Generate the binomial
+	binomial = generate_binomial_distribution(n_observations, p)
+	# Initialise x values
+	x = np.arange(binom.ppf(0.01, n_observations, p), \
+				  binom.ppf(0.99, n_observations, p))
+	# Plot binomial
 	plt.bar(x, binomial.pmf(x))
+	plt.vlines(n_exceptions, ymin=0, ymax=0.1, color='r', \
+			label="Observed # exceptions", linestyle='dashed')
 	plt.xlabel("Number of exceptions")
 	plt.ylabel("Probability")
 	plt.show()
-	return binomial
+	# Find p-value
+	p_value = binomial.cdf(n_exceptions)
+	return p_value 
+
+
